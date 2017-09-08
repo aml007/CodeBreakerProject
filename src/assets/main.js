2a3190c8b9e352e5afeade7c44a07ddfffd77a10
aml007 <albertomartinezlope@gmail.com>
2017-09-08 23:25:03 +0200

    Task 5 completed.

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,8 +2,28 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+  let input = document.getElementById('user-guess');
+  //add functionality to guess function here
 }
 
-//implement new functions here
+function setHiddenFields() {
+  answer.value = Math.floor(Math.random() * 10000);
+}
+
+// Test
+var test = function() {
+  const max = 10000; // 10000 not included
+  let numbers = [], sizes = [], i = 0, j = 0;
+
+  for (i; i < 100; i++) {
+    let number = Math.floor(Math.random() * max);
+    numbers.push(number);
+  }
+
+  for (j; j < max; j++) {
+    sizes.push(numbers.filter(function(value) {return value == j}).length);
+  }
+
+  console.log(numbers);
+  console.log(sizes);
+};
